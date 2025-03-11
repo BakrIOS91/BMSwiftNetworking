faff756c79745f3a97a89feff6e15b02c8b25fef
Bakr Mohamed Hassan <Engbakrmohamed@hotmail.com>
2025-03-11 23:55:05 +0200

    add swiftSettings

--- Package.swift
+++ Package.swift
@@ -22,7 +22,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BMSwiftNetworking"),
+            name: "BMSwiftNetworking",
+            swiftSettings: [
+                .define("SPM"),
+                .unsafeFlags(["-enable-library-evolution"])  // Enable library evolution
+            ]
+        ),
         .testTarget(
             name: "BMSwiftNetworkingTests",
             dependencies: ["BMSwiftNetworking"]),
